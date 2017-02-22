671e689122f64253c57e1ad4ef8e2b7e7fe412da
johntduong <johntduong@yahoo.com>
2017-02-22 14:46:54 -0800

    Passed up to Component Properties aka "props"

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 // TODO
+// class GroceryList extends React.Component {
+//   constructor() {
+//     super();
+//   }
+
+//   render() {
+//     return (
+//       <ul>
+//         <li>Milk</li>
+//         <li>Rice</li>
+//       </ul>
+//     )
+//   }
+// }
+
+var GroceryList = (props) => (
+  <ul>
+    <li>{props.todos[0]}</li>
+    <li>{props.todos[1]}</li>
+    <li>{props.todos[2]}</li>
+  </ul>
+);
+
+var App = () => (
+  <div>
+     <h2>My Shopping List</h2>
+     <GroceryList todos={['Milk', 'Water', 'Coke']} />
+  </div>
+);
+
+
+// var Cucumber = () => (
+//   <ul>
+//     <li>Cucumber</li>
+//   </ul>
+// );
+
+// var Kale = () => (
+//   <ul>
+//     <li>Kale</li>
+//   </ul>
+// );
+
+
+
+// var list = new App();
+
+//ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
